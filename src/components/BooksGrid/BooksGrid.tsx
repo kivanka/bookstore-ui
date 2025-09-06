22554baa5ee73ff9b7370f21680ce9b841122541
kivanka <kivaanka1@mail.ru>
2025-09-06 08:12:16 +0300

    init: bookstore UI

--- src/components/BooksGrid/BooksGrid.tsx
+++ src/components/BooksGrid/BooksGrid.tsx
@@ -2,7 +2,7 @@ import type { BookSummary } from '../../types/book'
 import BookCard from '../BookCard/BookCard'
 import s from './BooksGrid.module.css'
 
-type Props = { books: BookSummary[]; authors?: Record<string,string> } 
+type Props = { books: BookSummary[]; authors?: Record<string,string> }
 
 export default function BooksGrid({ books, authors = {} }: Props) {
   return (
